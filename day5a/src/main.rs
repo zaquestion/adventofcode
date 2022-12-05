c68da4452c347ba7bbb8e63fca3cd36c57fd978f
Zaq? Wiedmann <zaquestion@gmail.com>
2022-12-04 22:25:23 -0800

    day 5 oooooooof
    that was way rougher than need be

---
+++ day5a/src/main.rs
@@ -0,0 +1,52 @@
+use std::io;
+
+fn main() {
+    let lines: Vec<String> = io::stdin().lines()
+                                .filter_map(Result::ok).collect();
+    for line in lines.iter() {
+        println!("{:?}", line);
+    }
+
+    let mut stacks: Vec<Vec<char>> = Vec::new();
+    let (board, moves) = lines.split_at(9);
+
+
+
+    for col in (0..=8) {
+        let mut stack: Vec<char> = Vec::new();
+        let mut h: usize = 7;
+        loop {
+            if board[h].chars().nth(col*4) == Some('[') {
+                stack.push(board[h].chars().nth((col*4)+1).unwrap_or_default());
+            } else {
+                h = 8;
+                break;
+            }
+            if h == 0 {
+                h = 8;
+                break;
+            }
+            h  -= 1;
+        }
+        stacks.push(stack);
+
+    }
+
+
+
+    for m  in moves.iter().skip(1) {
+        let ms = m.split(' ').collect::<Vec<_>>();
+        let n = ms[1].parse::<i32>().unwrap_or_default();
+        let from = ms[3].parse::<usize>().unwrap_or_default();
+        let to = ms[5].parse::<usize>().unwrap_or_default();
+
+        for i in 1..=n {
+            let b = stacks[from-1].pop().unwrap();
+            stacks[to-1].push(b);
+        }
+    }
+    for stack in stacks.iter() {
+        print!("{}", stack.last().unwrap());
+    }
+
+}
