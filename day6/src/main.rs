ef52175d37efc1b969703baeb9c892cfa98fb9e7
Zaq? Wiedmann <zaq.wiedmann@outreach.io>
2022-12-05 23:16:30 -0800

    couple day 6 alt approaches -- experiments

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -25,9 +25,17 @@ fn part2(lines: &Vec<String>) {
     println!("invalid")
 }
 
+fn part1alts(lines: &Vec<String>) {
+    let line = &lines[0];
+    let uw = 4;
+    println!("{}", line.chars().enumerate().take_while(|(i, _)| line.chars().skip(*i).take(uw).collect::<HashSet<char>>().len().ne(&4)).count() + uw);
+    println!("{:?}", line.as_bytes().windows(4).take_while(|window|{ let h: HashSet<&u8> = HashSet::from_iter(window.iter()); h.len().ne(&4)}).count() + uw)
+}
+
 fn main() {
     let lines: Vec<String> = io::stdin().lines()
                                 .filter_map(Result::ok).collect();
     part1(&lines);
     part2(&lines);
+    part1alts(&lines);
 }
