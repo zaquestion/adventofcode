57433ba232444b0afab827c51d98aea857d3f0af
Zaq? Wiedmann <zaq.wiedmann@outreach.io>
2023-11-27 20:25:29 -0800

    day1 2021 warmup for 2023 bayyyyB

---
+++ 2021/day01/src/main.rs
@@ -0,0 +1,29 @@
+use itertools::Itertools;
+use std::io;
+
+fn main() {
+    let lines: Vec<String> = io::stdin().lines().filter_map(Result::ok).collect();
+
+    let nums = lines
+        .iter()
+        .map(|l| l.parse::<isize>().expect("must num"))
+        .collect::<Vec<isize>>();
+
+    let increases = nums.iter().tuple_windows::<(&isize, &isize)>().fold(
+        0,
+        |acc: isize, x: (&isize, &isize)| if x.0 < x.1 { acc + 1 } else { acc },
+    );
+
+    println!("part1: {}", increases);
+
+    let batch_increases = nums
+        .windows(3)
+        .map(|v| v[0] + v[1] + v[2])
+        .tuple_windows::<(isize, isize)>()
+        .fold(
+            0,
+            |acc: isize, x: (isize, isize)| if x.0 < x.1 { acc + 1 } else { acc },
+        );
+
+    println!("part2: {}", batch_increases);
+}
