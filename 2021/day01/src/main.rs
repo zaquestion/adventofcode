496e60a5ca0033e612011ac163556c44ef2d6195
Zaq? Wiedmann <zaq.wiedmann@outreach.io>
2023-11-27 21:08:39 -0800

    clean up folds

--- 2021/day01/src/main.rs
+++ 2021/day01/src/main.rs
@@ -9,10 +9,12 @@ fn main() {
         .map(|l| l.parse::<isize>().expect("must num"))
         .collect::<Vec<isize>>();
 
-    let increases = nums.iter().tuple_windows::<(&isize, &isize)>().fold(
-        0,
-        |acc: isize, x: (&isize, &isize)| if x.0 < x.1 { acc + 1 } else { acc },
-    );
+    let increases = nums
+        .iter()
+        .tuple_windows::<(&isize, &isize)>()
+        .fold(0, |acc: isize, x: (&isize, &isize)| {
+            acc + if x.0 < x.1 { 1 } else { 0 }
+        });
 
     println!("part1: {}", increases);
 
@@ -20,10 +22,9 @@ fn main() {
         .windows(3)
         .map(|v| v.iter().sum())
         .tuple_windows::<(isize, isize)>()
-        .fold(
-            0,
-            |acc: isize, x: (isize, isize)| if x.0 < x.1 { acc + 1 } else { acc },
-        );
+        .fold(0, |acc: isize, x: (isize, isize)| {
+            acc + if x.0 < x.1 { 1 } else { 0 }
+        });
 
     println!("part2: {}", batch_increases);
 }
