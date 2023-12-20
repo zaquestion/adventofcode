2e4f7442e43aa6d37f85d30ed88551dba9e79155
Zaq? Wiedmann <zaq.wiedmann@outreach.io>
2023-12-19 19:09:14 -0800

    reorder template to flow better with the order of an AoC problem

--- 2023/day00/src/main.rs
+++ 2023/day00/src/main.rs
@@ -1,33 +1,6 @@
 use itertools::Itertools;
 use std::io;
 
-fn main() {
-    let lines: Vec<String> = io::stdin().lines().filter_map(Result::ok).collect();
-    println!("part 1: {:?}", part1(&lines));
-    println!("part 2: {:?}", part2(&lines));
-}
-
-#[derive(Debug, PartialEq, Clone, Copy)]
-enum Cell {
-    Empty,
-}
-
-fn part1(lines: &Vec<String>) -> String {
-    let board = parse(lines);
-
-    format!("{}", todo!())
-}
-
-fn part2(lines: &Vec<String>) -> String {
-    let board = parse(lines);
-
-    format!("{}", todo!())
-}
-
-fn parse(lines: &Vec<String>) -> Vec<Vec<Cell>> {
-    todo!()
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -48,4 +21,31 @@ mod tests {
     fn test_part2_sample(#[case] input: Vec<String>, #[case] expected: String) {
         assert_eq!(expected, part2(&input));
     }
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum Cell {
+    Empty,
+}
+
+fn parse(lines: &Vec<String>) -> Vec<Vec<Cell>> {
+    todo!()
+}
+
+fn part1(lines: &Vec<String>) -> String {
+    let board = parse(lines);
+
+    format!("{}", todo!())
+}
+
+fn part2(lines: &Vec<String>) -> String {
+    let board = parse(lines);
+
+    format!("{}", todo!())
+}
+
+fn main() {
+    let lines: Vec<String> = io::stdin().lines().filter_map(Result::ok).collect();
+    println!("part 1: {:?}", part1(&lines));
+    println!("part 2: {:?}", part2(&lines));
 }
